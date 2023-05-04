a3fca77672b0513193421dac7c3bd87b5e579514
Miguel <mfg1014@alu.ubu.es>
2023-05-04 16:49:48 +0200

    [Extract Method] "logging" code refactored  Changes to be committed: 	modified:   src/lanSimulation/Network.java
    References issues #3

--- src/lanSimulation/Network.java
+++ src/lanSimulation/Network.java
@@ -176,13 +176,11 @@ which should be treated by all nodes.
 				report.write("\tNode '");
 				report.write(currentNode.name_);
 				report.write("' accepts broadcase packet.\n");
-				report.write("\tNode '");
-				report.write(currentNode.name_);
-				report.write("' passes packet on.\n");
 				report.flush();
 			} catch (IOException exc) {
 				// just ignore
 			};
+			extractedWriterNode(report, currentNode);
 			currentNode = currentNode.nextNode_;
 		} while (! packet.destination_.equals(currentNode.name_));
 
@@ -227,25 +225,11 @@ Therefore #receiver sends a packet across the token ring network, until either
 
 		startNode = (Node) workstations_.get(workstation);
 
-		try {
-			report.write("\tNode '");
-			report.write(startNode.name_);
-			report.write("' passes packet on.\n");
-			report.flush();
-		} catch (IOException exc) {
-			// just ignore
-		};
+		extractedWriterNode(report, startNode);
 		currentNode = startNode.nextNode_;
 		while ((! packet.destination_.equals(currentNode.name_))
 				& (! packet.origin_.equals(currentNode.name_))) {
-			try {
-				report.write("\tNode '");
-				report.write(currentNode.name_);
-				report.write("' passes packet on.\n");
-				report.flush();
-			} catch (IOException exc) {
-				// just ignore
-			};
+			extractedWriterNode(report, currentNode);
 			currentNode = currentNode.nextNode_;
 		};
 
@@ -262,6 +246,17 @@ Therefore #receiver sends a packet across the token ring network, until either
 		}
 
 		return result;
+	}
+
+	private void extractedWriterNode(Writer report, Node currentNode) {
+		try {
+			report.write("\tNode '");
+			report.write(currentNode.name_);
+			report.write("' passes packet on.\n");
+			report.flush();
+		} catch (IOException exc) {
+			// just ignore
+		};
 	}
 
 	private boolean printDocument (Node printer, Packet document, Writer report) {
