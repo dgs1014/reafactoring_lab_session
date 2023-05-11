561a6f6e62164dc59e6ec4852922611018dbe421
Miguel <mfg1014@alu.ubu.es>
2023-05-11 18:16:15 +0200

    [Replace Type Code] Call constructor  Changes to be committed: 	modified:   src/lanSimulation/Network.java 	modified:   src/lanSimulation/internals/Printer.java 	modified:   src/lanSimulation/internals/WorkStation.java  References Issues #11

--- src/lanSimulation/internals/Printer.java
+++ src/lanSimulation/internals/Printer.java
@@ -1,4 +1,10 @@
+package lanSimulation.internals;
 
 public class Printer extends Node {
+
+	public Printer(byte type, String name) {
+		super(type, name);
+		
+	}
 
 }
--- src/lanSimulation/internals/WorkStation.java
+++ src/lanSimulation/internals/WorkStation.java
@@ -1,4 +1,10 @@
+package lanSimulation.internals;
 
 public class WorkStation extends Node{
+
+	public WorkStation(byte type, String name) {
+		super(type, name);
+		
+	}
 
 }
